4178105a59c9dfef5b15e02b1d2abf51c051c718
limaarthur <artglima.dev@gmail.com>
2025-06-17 20:44:34 -0300

    feat: pages and routes - route layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Cart } from './pages/Cart'
 import { Success } from './pages/Success'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/checkout" element={<Cart />} />
-      <Route path="/success" element={<Success />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Cart />} />
+        <Route path="/success" element={<Success />} />
+      </Route>
     </Routes>
   )
 }
