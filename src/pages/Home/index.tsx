969279699248802a43ff8228715551978b90daeb
limaarthur <artglima.dev@gmail.com>
2025-06-19 23:15:54 -0300

    feat: pages and routes - applying colors usetheme function

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,9 +5,12 @@ import {
   TimerIcon,
 } from '@phosphor-icons/react'
 
+import { useTheme } from 'styled-components'
 import { Heading, Hero, HeroContent, Info } from './styles'
 
 export function Home() {
+  const theme = useTheme()
+
   return (
     <div>
       <Hero>
@@ -26,22 +29,42 @@ export function Home() {
 
             <Info>
               <div>
-                <ShoppingCartIcon size={32} weight="fill" />
+                <ShoppingCartIcon
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors['yellow-dark'] }}
+                />
                 <span>Compra simples e segura</span>
               </div>
 
               <div>
-                <PackageIcon size={32} weight="fill" />
+                <PackageIcon
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors['base-text'] }}
+                />
                 <span>Embalagem mantém o café intacto</span>
               </div>
 
               <div>
-                <TimerIcon size={32} weight="fill" />
+                <TimerIcon
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors.yellow }}
+                />
                 <span>Entrega rápida e rastreada</span>
               </div>
 
               <div>
-                <CoffeeIcon size={32} weight="fill" />
+                <CoffeeIcon
+                  size={32}
+                  weight="fill"
+                  color={theme.colors.background}
+                  style={{ backgroundColor: theme.colors.purple }}
+                />
                 <span>O café chega fresquinho até você</span>
               </div>
             </Info>
